ee7e6c6f8a76e4ff44b0d2de3e81f400167b6a19
Varun Malaviya <varunmalaviya7@gmail.com>
2023-04-30 15:06:04 -0400

    added uuid as a dependeny to create id for products. Created a products array for hardcoded products to use on working with styling for now.

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,6 +1,22 @@
+import { Col, Row } from "react-bootstrap"
+import { products } from "../products"
+
 function Store(){
+
+    //array conating products for sale mapped into columns
+    const productElements = products.map((product,index) =>
+     <Col key={index} id={product.id} align='center'>
+        <h1>{product.name}</h1>
+        <h4>${product.price}</h4>
+     </Col> )
+
     return(
-        <h1>Welcome to our Store!</h1>
+        <div className="store">
+            <h1 className="p-4">Welcome to our Store!</h1>
+            <Row xs={1} md={3} className="g-5">
+                {productElements}
+            </Row>
+        </div>
     )
 }
 
---
+++ src/products.js
@@ -0,0 +1,42 @@
+//hardcoded array of products to be displayed in our store
+import { v4 as uuidv4 } from 'uuid';
+
+const products = [
+    {
+        id: uuidv4,
+        name: "Coffee",
+        price: 4.99
+    },
+    {
+        id: uuidv4,
+        name: "Pens",
+        price: 4.99
+    },
+    {
+        id: uuidv4,
+        name: "Laptop",
+        price: 499.99
+    },
+    {
+        id: uuidv4,
+        name: "Lamp",
+        price: 14.99
+    },
+    {
+        id: uuidv4,
+        name: "Chair",
+        price: 44.99
+    },
+    {
+        id: uuidv4,
+        name: "Phone",
+        price: 444.99
+    },
+]
+
+function findProductByID(id){
+    let selected_product = products.find(product => product.id === id)
+    return selected_product
+}
+
+export { products, findProductByID }
