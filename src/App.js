280863013b5bf01cddd97f1c63297bde8dbfa13e
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-01 15:37:13 -0400

    created cart context fucntions for adding,removing, deleting items from cart + calculating total cost i=of all items in cart. Added Cart context in app,js to provide that context for all elements in our app.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Success from './pages/Success';
 import Store from './pages/Store';
 import Cancel from './pages/Cancel';
+import { CartProvider } from './CartContext';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <NavBarComponent />
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Store />}/>
-            <Route path='success' element={<Success />}/>
-            <Route path='cancel' element={<Cancel />}/>
-          </Routes>
-        </BrowserRouter>
-      </Container>
+      <CartProvider>
+        <Container>
+          <NavBarComponent />
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Store />}/>
+              <Route path='success' element={<Success />}/>
+              <Route path='cancel' element={<Cancel />}/>
+            </Routes>
+          </BrowserRouter>
+        </Container>
+      </CartProvider>
     </div>
   );
 }
--- src/CartContext.js
+++ src/CartContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { products } from "./products";
+import { findProductByID } from "./products";
 
 const CartContext = createContext({
     items: [],
@@ -13,6 +13,81 @@ const CartContext = createContext({
 function CartProvider({ children }){
 
     const [productsInCart, setProductsInCart] = useState([])
+
+    //function for grabbing item quantity from id of element being clicked
+    function itemQuantity(id){
+        const quantity = productsInCart.find(product => product.id === id)?.quantity 
+
+        if(quantity === undefined){
+            return 0
+        }
+
+        return quantity
+
+    }
+
+    //function to add one item to cart
+    function addOne(id){
+        const quantity = itemQuantity(id)
+
+        if(quantity === 0){   
+            //item has not been added to the cart previously
+            setProductsInCart([...productsInCart, { id: id, quantity: 1 }])
+        } else {              
+            //item is in the cart already 
+            setProductsInCart(
+                productsInCart.map(
+                    item =>
+                    item.id === id 
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+                )
+            )
+        }
+    }
+
+    //function for deleting all items of a particular id from the cart
+    function deleteOne(id){
+        //using filter to remove items matching given id
+        setProductsInCart(
+            productsInCart.filter(
+                item =>
+                item.id !== id
+            )
+        )
+    }
+
+    //function for removing an item from cart
+    function removeOne(id){
+        const quantity = itemQuantity(id)
+
+        if(quantity === 1){   
+            //only one item in the cart
+            deleteOne(id)
+        } else {              
+            //item is in the cart already 
+            setProductsInCart(
+                productsInCart.map(
+                    item =>
+                    item.id === id 
+                    ? { ...item, quantity: item.quantity - 1 }
+                    : item
+                )
+            )
+        }
+    }
+
+    //function for calculating total cost of all items in cart
+    function totalCost(){
+        let totalCost = 0
+        productsInCart.forEach(
+            item => {
+                const itemData = findProductByID(item.id)
+                totalCost += (itemData.price * item.quantity)
+            }
+        )
+        return totalCost
+    }
 
     const contextValue = {
         items: productsInCart,
